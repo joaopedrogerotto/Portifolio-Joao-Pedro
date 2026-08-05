79c9743c9bf59e844852f34fa0fcf8b363d245c0
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2026-08-05 13:39:24 -0300

    feat: efeito de carregar a pagina conforme scroll

--- javascript/hidden.js
+++ javascript/hidden.js
@@ -1,10 +1,12 @@
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach(entry =>{
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
         if(entry.isIntersecting){
             entry.target.classList.add("show");
         }
-    })
-})
+    });
+},{
+    threshold: 0.3
+});
 
 const hiddenElements = document.querySelectorAll(".hidden");
 
