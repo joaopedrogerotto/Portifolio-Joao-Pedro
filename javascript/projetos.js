7c0f796b4d0d10c4a55fa22a8a5c82adfeee2a8b
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2026-08-05 16:42:38 -0300

    feat: adicionado projetos

---
+++ javascript/projetos.js
@@ -0,0 +1,29 @@
+fetch("../projects.json")
+    .then(response => response.json())
+    .then(projects => {
+
+        const listaProjetos = document.getElementById("lista-projetos");
+
+        projects.forEach(project => {
+
+            const tecnologias = project.tecnologia
+                .map(t => `<span>${t}</span>`)
+                .join("");
+
+            listaProjetos.insertAdjacentHTML("beforeend", `
+                <div class="card-projeto">
+                    <h3>${project.nome}</h3>
+
+                    <div class="tecnologias">
+                        ${tecnologias}
+                    </div>
+
+                    <a href="${project.github}" target="_blank" title="Ver no GitHub">
+                        <i class="devicon-github-original"></i>
+                    </a>
+                </div>
+            `);
+
+        });
+
+    });
